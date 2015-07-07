e6668af08c49fe74f306bdf6469859683bfe6e7c
Steven <stevenboada@gmail.com>
2015-07-07 11:58:15 -0700

    clean up

--- sandbox/legacy_plot_code/printstuff.py
+++
@@ -1,7 +0,0 @@
-import cPickle as pickle
-galaxies = pickle.load(open('galaxies.pickle','rb'))
-galaxies = filter(lambda galaxy: galaxy.ston_I > 30., galaxies)
-
-for galaxy in galaxies:
-    print int(galaxy.ID), galaxy.RA, galaxy.DEC
-
